3e5912574e55719135b926662f9d57095a89fb46
Valeska Valentin Ekklesia <106671548+ValeskaLim@users.noreply.github.com>
2024-10-07 13:36:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,21 +13,21 @@ YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
 CHANNELS = [
     {"channel_id": "UCMwGHR0BTZuLsmjY_NT5Pwg", "name": "Ninomae Ina'nis"},
     {"channel_id": "UC8rcEBzJSleTkf_-agPM20g", "name": "IRyS"},
-    {"channel_id": "UCl69AEx4MdqMZH7Jtsm7Tig", "name": "Raora Panthera"},
+    # {"channel_id": "UCl69AEx4MdqMZH7Jtsm7Tig", "name": "Raora Panthera"},
     {"channel_id": "UC9p_lqQ0FEDz327Vgf5JwqA", "name": "Koseki Bijou"},
     {"channel_id": "UCgmPnx-EEeOrZSg5Tiw7ZRQ", "name": "Hakos Baelz"},
     {"channel_id": "UCL_qhgtOy0dy1Agp8vkySQg", "name": "Mori Calliope"},
-    {"channel_id": "UCHsx4Hqa-1ORjQTh9TYDhww", "name": "Takanashi Kiara"},
-    {"channel_id": "UC_sFNM0z0MWm9A6WlKPuMMg", "name": "Nerissa Ravencroft"},
-    {"channel_id": "UCgnfPPb9JI3e9A4cXHnWbyg", "name": "Shiori Novella"},
+    # {"channel_id": "UCHsx4Hqa-1ORjQTh9TYDhww", "name": "Takanashi Kiara"},
+    # {"channel_id": "UC_sFNM0z0MWm9A6WlKPuMMg", "name": "Nerissa Ravencroft"},
+    # {"channel_id": "UCgnfPPb9JI3e9A4cXHnWbyg", "name": "Shiori Novella"},
     {"channel_id": "UC3n5uGu18FoCy23ggWWp8tA", "name": "Nanashi Mumei"},
-    {"channel_id": "UCvN5h1ShZtc7nly3pezRayg", "name": "Cecilia Immergreen"},
-    {"channel_id": "UCDHABijvPBnJm7F-KlNME3w", "name": "Gigi Murin"},
+    # {"channel_id": "UCvN5h1ShZtc7nly3pezRayg", "name": "Cecilia Immergreen"},
+    # {"channel_id": "UCDHABijvPBnJm7F-KlNME3w", "name": "Gigi Murin"},
     {"channel_id": "UCO_aKKYxn4tvrqPjcTzZ6EQ", "name": "Ceres Fauna"},
     {"channel_id": "UCt9H_RpQzhxzlyBxFqrdHqA", "name": "FUWAMOCO"},
     {"channel_id": "UCmbs8T6MWqUHP1tIQvSgKrg", "name": "Ouro Kronii"},
     {"channel_id": "UCoSrY_IQQVpmIRZ9Xf-y93g", "name": "Gawr Gura"},
-    {"channel_id": "UCW5uhrG1eCBYditmhL0Ykjw", "name": "Elizabeth Rose "},
+    # {"channel_id": "UCW5uhrG1eCBYditmhL0Ykjw", "name": "Elizabeth Rose "},
 ]
 
 # Paste your Chat ID Telegram Bot here
@@ -81,7 +81,7 @@ def main():
     job_queue = app.job_queue
 
     # Adjust the interval of your bots (in seconds)
-    job_queue.run_repeating(periodic_check, interval=7200, first=10)
+    job_queue.run_repeating(periodic_check, interval=10800, first=10)
 
     print("Telegram bot started!", flush=True)
     app.run_polling()
